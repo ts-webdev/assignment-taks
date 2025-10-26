72733aace42e49e7c27cfc3ccb8b2bde8b32f437
ts-webdev <dev.tanvirmahtab@gmail.com>
2025-10-26 12:07:41 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <header className="flex justify-center py-5 bg-base-300">
+        <h1 className="text-2xl font-bold">Assignment Tasks</h1>
+      </header>
+    </>
+  );
+}
+
+export default App;
